f867743e92823823cf6e4654c260ce4560fff2d3
selemist <168042467+selemist@users.noreply.github.com>
2024-08-15 19:27:33 -0300

    Agregué las funcionalidades en contraseña y repetir contraseña

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,21 @@
+document.getElementById('regBtn').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const password1 = document.getElementById('password1').value;
+    const password2 = document.getElementById('password2').value;
+
+    hideAlerts();
+
+    if (password1 !== password2) {
+        showAlertError();
+    }
+    else if (password1.length < 6 ){
+        showAlertError();
+    }
+    else {
+        showAlertSuccess();
+    }
+    
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -5,3 +23,4 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+})
